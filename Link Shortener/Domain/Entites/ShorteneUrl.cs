5b6c15ff477dab26c2834ef261df5687a6bc9265
whoshington <whoshington56@gmail.com>
2026-04-28 13:54:19 -0300

    Creating the properties and the constructor

---
+++ Link Shortener/Domain/Entites/ShorteneUrl.cs
@@ -0,0 +1,22 @@
+﻿namespace Url_Shortener.Domain.Entites
+{
+    public class ShorteneUrl
+    {
+        public int Id { get; set; }
+        public string ShortUrl { get; set; }
+        public string LongUrl { get; set; }
+        public DateTime CreationUrl { get; set; } = DateTime.Now;
+        public DateTime ExpirationUrl { get; set; } 
+
+        public ShorteneUrl() { }
+        public ShorteneUrl(string shortUrl, string longUrl, DateTime creationUrl)
+        {
+            ShortUrl = shortUrl;
+            LongUrl = longUrl;
+            CreationUrl = creationUrl;
+
+            // Set the expiration date to 30 days after the creation date
+            ExpirationUrl = CreationUrl.AddDays(30);
+        }
+    }
+}
